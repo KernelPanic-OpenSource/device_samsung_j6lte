6b54d32063ba2437af2abc075d3a5731cda90aed
dopaemon <polarisdp@gmail.com>
2020-08-16 14:04:52 +0700

    CherishOS: Init Commit Support CherishOS
    Wait Official
    
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- cherish_j6lte.mk
+++ cherish_j6lte.mk
@@ -22,12 +22,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from j6lte device
 $(call inherit-product, device/samsung/j6lte/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CherishOS stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j6lte
-PRODUCT_NAME := lineage_j6lte
+PRODUCT_NAME := cherish_j6lte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 
